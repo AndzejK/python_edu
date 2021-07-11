5bb6a75d84bbb06aa301113e3844a7353d878b52
MAC <andrejkrupoves@gmail.com>
2021-07-11 11:29:09 +1000

    simple browser

---
+++ networking.py
@@ -0,0 +1,13 @@
+#A simple browaser
+import socket
+mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80)) #connect to/ extend the connection to. Like a phone call
+cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data=mysock.recv(512)
+    if(len(data)<1):
+        break
+    print(data.decode())
+mysock.close()
