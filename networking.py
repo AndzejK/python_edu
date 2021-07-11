ed8dc344f16975b1d666ab1454cb3133f8729ebe
MAC <andrejkrupoves@gmail.com>
2021-07-11 13:54:28 +1000

    a little update

--- networking.py
+++ networking.py
@@ -2,11 +2,11 @@
 import socket
 mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org',80)) #connect to/ extend the connection to. Like a phone call
-cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode() #we sending here bites Not string
 mysock.send(cmd)
 
 while True:
-    data=mysock.recv(512)
+    data=mysock.recv(512) #512 means up to 512 characters at a time
     if(len(data)<1):
         break
     print(data.decode())
