2248a4a2a43a891836523aa3cb35eaab0c44ef7a
MAC <andrejkrupoves@gmail.com>
2021-07-11 14:09:23 +1000

    extracting achor links from a page

--- txt_processing.py
+++ txt_processing.py
@@ -2,7 +2,7 @@
 #print(ord("A"))
 #urllib even more simple browser
 import urllib.request,urllib.parse,urllib.error
-import bs4
+import bs4, ssl
 fhand=urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
     print(line.decode().strip()) #since we get back data in bites we need to decode data 
@@ -11,10 +11,17 @@ print('-'*15)
 #for line in fhand:
 #    print(line.decode().strip())
 
-url = input('Enter - ')
-html=urllib.request.urlopen(url).read()
-soup=bs4.BeautifulSoup(html,'html.parser')
 
-tags=soup('a')
+#Get all hyperlink from a current page
+# Ignore SSL cert errors
+ctx=ssl.create_default_context()
+ctx.check_hostname=False
+ctx.verify_mode=ssl.CERT_NONE
+
+url = input('Enter - ')
+html=urllib.request.urlopen(url, context=ctx).read() #returns entire web-page 
+soup=bs4.BeautifulSoup(html,'html.parser') #getting back an object
+
+tags=soup('a') #asking for anchor tag <a href=""></a>
 for tag in tags:
     print(tag.get('href',None))
