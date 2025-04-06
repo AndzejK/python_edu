996e3da3f46684adaab55b4f2f83063365882d6e
Andzej Krupoves <andrejkrupovec@gmail.com>
2025-04-06 18:28:31 +0300

    diving deeper into OOP

--- classesObjects.py
+++ classesObjects.py
@@ -6,10 +6,15 @@ class Person:
   def __str__(self):
      return (f"A new instance has been created: {self.name}({(self.age)})")
 
+  # method that will belog to an obj
+  def my_fn(self):
+    print(f"Hey there, my name is {self.name}")
+
 p1 = Person("John", 36)
-
-
-
+print(p1)
+print(p1.name)
+print(p1.my_fn)
+p1.my_fn()
 # class Person:
 #   def __init__(self, name, age):
 #     self.name = name
