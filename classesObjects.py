859d257d67b9b3fe28d13e652e4aaa4030d8b187
Andzej Krupoves <andrejkrupovec@gmail.com>
2025-04-07 06:40:09 +0300

    minor updates

--- classesObjects.py
+++ classesObjects.py
@@ -1,19 +1,20 @@
 class Person:
   # self - Reference to the instance of the Person class. Used to access instance variables and methods. The parameter name could be different, but 'self' is conventional in Python.
   def __init__(self, name, age):
-    self.name = name
+    self.firstname = name
     self.age = age
 
   def __str__(self):
-     return (f"A new instance has been created: {self.name}({(self.age)})")
+     return (f"A new instance has been created: {self.firstname}({(self.age)})")
 
   # method that will belog to an obj
   def my_fn(self):
-    print(f"Hey there, my name is {self.name}")
+    print(f"Hey there, my name is {self.firstname} and I'm {self.age} years old.")
 
 p1 = Person("John", 36)
-print(p1)
-print(p1.name)
+
+p1.age=34
+print(p1.firstname)
 print(p1.my_fn)
 p1.my_fn()
 # class Person:
