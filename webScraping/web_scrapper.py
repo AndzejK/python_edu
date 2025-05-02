71d86dea0b0938806c05af156ebf76b99f71f8af
Andzej Krupoves <andrejkrupovec@gmail.com>
2025-05-02 21:45:44 +0300

    Pre-configuring web scraping project

--- webScraping/web_scrapper.py
+++ webScraping/web_scrapper.py
@@ -2,20 +2,19 @@ import requests as req
 from bs4 import BeautifulSoup
 
 
-web = req.get("https://www.livinn.lt")
 
-web_url_no_item = req.get("https://www.livinn.lt/p/keltu-juros-druska-celtic-sea-salt-1-kg-cupp7153-lt")
+# START: Target Site
+web_raskakcija = 'https://www.raskakcija.lt'
+web_raskakcija_robots = 'https://www.raskakcija.lt/sitemap.xml'
 
-web_url_w_item = req.get("https://www.livinn.lt/p/keltu-juros-druska-celtic-sea-salt-rupi-454-g-celt0008-lt")
-web_url_string=web_url_w_item.text
+# rules:
+"""
+User-agent: *
+Crawl-delay: 10
+sitemap: https://www.raskakcija.lt/sitemap.xml
+"""
 
-# 1. HTML text (a string) is converted into a tree/ Beautiful Soup OBJECT with the help a parser such as 'lxml'
-soup = BeautifulSoup(web_url_string,'lxml')
+web = req.get(web_raskakcija)
 
-# 2. Now I can use Beautiful Soup's methods to work on this PARSE TREE/DOM
-find_div=soup.find_all("div", class_="cart__control-container")
-print(find_div)
+print(web)
 
-# <div cart__control-block active error
-
-# print(web_url.text)
