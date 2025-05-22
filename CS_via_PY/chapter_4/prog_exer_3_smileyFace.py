daec2367138c6bb5132fa4ca65aeb210da546764
Andzej Krupoves <andrejkrupovec@gmail.com>
2025-05-22 06:38:22 +0300

    drawing semi-face

--- CS_via_PY/chapter_4/prog_exer_3_smileyFace.py
+++ CS_via_PY/chapter_4/prog_exer_3_smileyFace.py
@@ -10,6 +10,7 @@ def main():
 
     head = Circle(Point(50,50),50)
     head.setFill(color_rgb(232, 190,172))
+
     left_eye=Circle(Point(38,65),8)
     left_eye.setFill("white")
     pupil_left=Circle(left_eye.getCenter(),1)
@@ -18,15 +19,17 @@ def main():
     right_eye=left_eye.clone()
     right_eye.move(25,0)
     pupil_right=pupil_left.clone()
-    
+    pupil_right.move(25,0)
 
-    
+    nose=Circle(head.getCenter(),4) #Point(50,50)
+    nose.setFill(color_rgb(199,122,88))
     
     head.draw(win)
     left_eye.draw(win)
     right_eye.draw(win)
     pupil_left.draw(win)
     pupil_right.draw(win)
+    nose.draw(win)
 
     win.getMouse()
 
